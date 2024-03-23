962113629fd7f68cf4dff26935a089de6728a507
FlavioDEV <flavio.henrique.dev@gmail.com>
2024-03-23 17:12:17 -0300

    ajustes

--- src/components/pages/administracao_sistema/indiceIPCAE/IndiceIPCAEComponente.tsx
+++ src/components/pages/administracao_sistema/indiceIPCAE/IndiceIPCAEComponente.tsx
@@ -1,28 +1,124 @@
 'use client'
 
-import React from "react";
+import {Pagesection} from "@/components/layout/pagesection";
+import React, {useCallback, useEffect, useState} from "react";
+import {toast} from "react-hot-toast";
+import {EntidadePadrao} from "@/class/EntidadePadrao";
 import {AxiosResponse} from "axios";
+import TabelaComponente from "@/components/datadisplay/tabelacomponente/TabelaComponente";
 import {IndiceIPCAE} from "@/class/IndiceIPCAE";
 import {excluirIndiceIPCAE, getIndiceIPCAE, salvarIndiceIPCAE} from "@/services/sdk/indiceIPCAE";
-import {ComponenteDePagina} from "@/components/componente_de_pagina/ComponenteDePagina";
+import {
+    IndiceIPCAEFormularioCadastro
+} from "@/components/pages/administracao_sistema/indiceIPCAE/IndiceIPCAEFormularioCadastro";
 import {indiceIPCAEColunasTabela} from "@/components/pages/administracao_sistema/indiceIPCAE/indiceIPCAEColunasTabela";
 
 const classeEntidade = IndiceIPCAE;
 const tituloPagina: string = 'Indice IPCAE'
 const tituloFormulario: string = 'Cadastro de Indice IPCAE'
-const funcaoFechCadastros: (filtro?: string, take?: string, skip?: string) => Promise<AxiosResponse<any, any>> = getIndiceIPCAE
+const funcaoFechCadastros: (take: string, skip: string) => Promise<AxiosResponse<any, any>> = getIndiceIPCAE
 const funcaoSalvarCadastro: (entidade: any) => Promise<AxiosResponse<any, any>> = salvarIndiceIPCAE
 const funcaoExcluirCadastro: (id: number) => Promise<AxiosResponse<any, any>> = excluirIndiceIPCAE
 
 export function IndiceIPCAEComponente() {
+    const [entidade, setEntidade] = useState<InstanceType<typeof classeEntidade>>(new (classeEntidade as any)());
+    const [listaEntidade, setListaEntidade] = useState<Array<InstanceType<typeof classeEntidade>>>([]);
+    const [editavel, setEditavel] = useState<boolean>(false)
+    const [acaoExecutada, setAcaoExecutada] = useState<string>('')
+    
+    const [take, setTake] = useState<number>(15)
+    const [skip, setSkip] = useState<number>(0)
+    const [totalRegistros, setTotalRegistros] = useState<number>()
+    
+    useEffect(() => {
+        setTake(15)
+        setSkip(0)
+    }, []);
+    
+    const fetchCadastros = useCallback(async () => {
+        const response = await funcaoFechCadastros(String(take), String(skip))
+            .then(res => {
+                return res.data
+            }).catch((error) => {
+                toast.error(error.message);
+            })
+        if (response) {
+            setListaEntidade(response.registros)
+            setTotalRegistros(response.quantRegistros);
+        }
+    }, [skip, take])
+    
+    useEffect(() => {
+        fetchCadastros()
+    }, [skip, take, fetchCadastros]);
+    
+    async function handleSalvarEntidade() {
+        await funcaoSalvarCadastro(entidade)
+            .then(res => {
+                toast.success(res.data.mensagem)
+            }).catch(error => {
+                toast.error(error.response.data.mensagem);
+            });
+        await fetchCadastros();
+        setEntidade(new (classeEntidade as any)());
+    }
+    
+    async function handleExcluirEntidade(id: number) {
+        await funcaoExcluirCadastro(id)
+            .then(res => {
+                return toast.success(res.data.mensagem)
+            }).catch(error => {
+                toast.error(error.data);
+            });
+        setListaEntidade(prevListaEntidade =>
+            prevListaEntidade.filter(entidadeFiltrada => entidadeFiltrada.id !== id));
+    }
+    
+    function novoCadastro() {
+        setEntidade(new (classeEntidade as any)())
+        setEditavel(true)
+    }
+    
+    function handleSelecionarEntidade<T extends EntidadePadrao>(entidade: InstanceType<typeof classeEntidade>) {
+        setAcaoExecutada('selecionar')
+        setEditavel(false)
+        setEntidade(entidade)
+    }
+    
+    function handleEditarEntidade(entidade: InstanceType<typeof classeEntidade>) {
+        setAcaoExecutada('editar')
+        setEditavel(true)
+        setEntidade(entidade)
+    }
+    
+    function clearModal() {
+        setEntidade(new (classeEntidade as any)())
+    }
+    
     return (
-        <ComponenteDePagina classeEntidade={classeEntidade}
-                            tituloPagina={tituloPagina}
-                            tituloFormulario={tituloFormulario}
-                            funcaoFechCadastros={funcaoFechCadastros}
-                            funcaoSalvarCadastro={funcaoSalvarCadastro}
-                            funcaoExcluirCadastro={funcaoExcluirCadastro}
-                            colunas={indiceIPCAEColunasTabela()}
-        />
+        <Pagesection.Container titulo={tituloPagina}
+                               tituloComponenteCadastro={tituloFormulario}
+                               totalDeRegistros={totalRegistros}
+                               acaoExecutada={acaoExecutada}
+                               entidade={entidade}
+                               clearModal={clearModal}
+                               funcaoNovoCadastro={novoCadastro}
+                               funcaoSalvarCadastro={handleSalvarEntidade}
+                               componenteCadastro={<IndiceIPCAEFormularioCadastro entidade={entidade}
+                                                                                  editavel={editavel}/>}
+        >
+            <TabelaComponente colunas={indiceIPCAEColunasTabela()}
+                              listaEntidade={listaEntidade}
+                              skip={skip}
+                              setSkip={setSkip}
+                              setTake={setTake}
+                              take={take}
+                              totalRegistros={totalRegistros}
+                              acoes={{
+                                  excluir: handleExcluirEntidade,
+                                  editar: handleEditarEntidade,
+                                  selecionar: handleSelecionarEntidade
+                              }}/>
+        </Pagesection.Container>
     )
 }
